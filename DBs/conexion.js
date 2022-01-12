d7b768ebf2c5d27343c7e8b88a8397c9d7dde69e
“ArmandoHernandezCastro” <armandohernandezcastro102@gmail.com>
2022-01-11 22:16:27 -0600

    ajuste en la variable de usuario de la BD

---
+++ DBs/conexion.js
@@ -0,0 +1,15 @@
+const mariadb = requiere("mariadb");
+
+const config = {
+    host: process.env.HOST,
+    user: process.env.DBUSER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    connectionLimit: process.env.CONN_LIMIT, 
+};
+
+console.log(config);
+
+const pool = mariadb.createPool(config);
+
+module.exports = pool;
